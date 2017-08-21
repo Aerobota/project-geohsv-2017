0728f3f49d7d470038c600c714bb6704c7c28a89
alexrobin <alex.robin@sensiasoft.com>
2017-08-20 23:10:07 -0500

    Enabled debug logging

--- osh-node-cloud/scripts/launch.sh
+++ osh-node-cloud/scripts/launch.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-java -Xmx384m -cp "lib/*" -Djava.system.class.loader="org.sensorhub.utils.NativeClassLoader" -Dlogback.configurationFile=./logback.xml org.sensorhub.impl.SensorHub config.json db
+now=`date +%Y%m%d_%H%M%S`
+logFile="log_$now.txt"
+java -Xmx384m -cp "lib/*" -Djava.system.class.loader="org.sensorhub.utils.NativeClassLoader" -Dlogback.configurationFile=./logback.xml org.sensorhub.impl.SensorHub config.json db | tee $logFile
--- osh-node-indoor/scripts/launch.sh
+++ osh-node-indoor/scripts/launch.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-java -Xmx384m -cp "lib/*" -Djava.system.class.loader="org.sensorhub.utils.NativeClassLoader" -Dlogback.configurationFile=./logback.xml -Djava.security.policy=./java.policy.ext.rpi -Dgnu.io.rxtx.SerialPorts=/dev/ttyAMA0:/dev/ttyACM0:/dev/ttyUSB0 org.sensorhub.impl.SensorHub config.json db
+now=`date +%Y%m%d_%H%M%S`
+logFile="log_$now.txt"
+java -Xmx384m -cp "lib/*" -Djava.system.class.loader="org.sensorhub.utils.NativeClassLoader" -Dlogback.configurationFile=./logback.xml -Djava.security.policy=./java.policy.ext.rpi -Dgnu.io.rxtx.SerialPorts=/dev/ttyAMA0:/dev/ttyACM0:/dev/ttyUSB0 org.sensorhub.impl.SensorHub config.json db | tee $logFile
